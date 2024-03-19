03555ef842bb18baac2b243ae55192d59021490b
Daria <157281966+Daria111118@users.noreply.github.com>
2024-03-19 15:04:22 +0600

    Update day/night

--- Assets/Day & Night Circle/DayAndNightCycle.cs
+++ Assets/Day & Night Circle/DayAndNightCycle.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
 
 public class DayAndNightCykle : MonoBehaviour
@@ -13,15 +15,23 @@ public class DayAndNightCykle : MonoBehaviour
     [Header("CurrentTime")]
     public string currentTimeString;
 
-    [Header("Light Settings")]
+    [Header("Sun Settings")]
     public Light sunLight;
     public float sunPosition = 1f;
     public float sunIntensity = 1f;
     public AnimationCurve sunIntensityMultiplier;
-    public AnimationCurve LightTemperatureCurve;
+    public AnimationCurve sunTemperatureCurve;
 
 
     public bool isDay = true;
+    public bool sunActive = true;
+    public bool moonActive = true;
+
+    [Header("Moon Settings")]
+    public Light moonLight;
+    public float moonIntensity = 1f;
+    public AnimationCurve moonIntensityMultiplier;
+    public AnimationCurve moonTemperatureCurve;
     void Start()
     {
         UpdateTimeText();
@@ -56,40 +66,83 @@ public class DayAndNightCykle : MonoBehaviour
     {
         float sunRotation = currentTime / 24f * 360f;
         sunLight.transform.rotation = Quaternion.Euler(sunRotation - 90f, sunPosition, 0f);
+        moonLight.transform.rotation = Quaternion.Euler(sunRotation + 90f, sunPosition, 0f);
+
 
         float normalizedTime = currentTime / 24f;
-        float intensityCurve = sunIntensityMultiplier.Evaluate(normalizedTime);
+        float sunIntensityCurve = sunIntensityMultiplier.Evaluate(normalizedTime);
+        float moonIntensityCurve = moonIntensityMultiplier.Evaluate(normalizedTime);
 
         HDAdditionalLightData sunLightData = sunLight.GetComponent<HDAdditionalLightData>();
+        HDAdditionalLightData moonLightData = moonLight.GetComponent<HDAdditionalLightData>();
 
         if (sunLightData != null )
         {
-            sunLightData.intensity = intensityCurve * sunIntensity;
+            sunLightData.intensity = sunIntensityCurve * sunIntensity;
         }
 
-        float temperatureMultiplier = LightTemperatureCurve.Evaluate(normalizedTime);
-        Light lightComponent = sunLight.GetComponent<Light>();
+        if (moonLightData != null)
+        {
+            moonLightData.intensity = moonIntensityCurve * moonIntensity;
+        }
 
-        if (lightComponent != null)
+        float sunTemperatureMultiplier = sunTemperatureCurve.Evaluate(normalizedTime);
+        float moonTemperatureMultiplier = moonTemperatureCurve.Evaluate(normalizedTime);
+        Light sunLightComponent = sunLight.GetComponent<Light>();
+        Light moonLightComponent = moonLight.GetComponent<Light>();
+
+        if (sunLightComponent != null)
         { 
-           lightComponent.colorTemperature = temperatureMultiplier * 10000f;
+           sunLightComponent.colorTemperature = sunTemperatureMultiplier * 10000f;
+        }
+
+        if (moonLightComponent != null)
+        {
+            moonLightComponent.colorTemperature = moonTemperatureMultiplier * 10000f;
         }
     }
 
     void CheckShadowStatus()
     {
         HDAdditionalLightData sunLightData = sunLight.GetComponent<HDAdditionalLightData>();
+        HDAdditionalLightData moonLightData = moonLight.GetComponent<HDAdditionalLightData>();
         float currentSunRotation = currentTime;
         if (currentSunRotation >= 6f && currentSunRotation <= 18f)
         {
             sunLightData.EnableShadows(true);
+            moonLightData.EnableShadows(false);
             isDay = true;
         }
 
         else 
         {
             sunLightData.EnableShadows(false);
+            moonLightData.EnableShadows(true);
             isDay = false;
+        }
+
+        if (currentSunRotation >= 5.7f && currentSunRotation <= 18.3f)
+        {
+            sunLight.gameObject.SetActive(true);
+            sunActive = true;
+        }
+
+        else
+        { 
+            sunLight.gameObject.SetActive(false);
+            sunActive = false;
+        }
+
+        if (currentSunRotation >= 6.3f && currentSunRotation <= 17.7f)
+        {
+            moonLight.gameObject.SetActive(false);
+            moonActive = false;
+        }
+
+        else
+        {
+            moonLight.gameObject.SetActive(true);
+            moonActive = true;
         }
     }
 }
